0349855c3637dde27ca32a5fda9940b6550ecc97
kazuya002ex <chiharu.event2@gmail.com>
2021-03-16 16:33:21 +0900

    feat: controller作成完了

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,5 +1,29 @@
 class MaterialsController < ApplicationController
 
   def create
+    @ear_age = EarAge.new(ear_age_params)
+    redirect_to root_path if @ear_age.save 
+  end
+
+  private
+
+  def ear_age_params
+    params.require(:ear_age).permit(:age,
+                                    :gender,
+                                    :job,
+                                    :other_job,
+                                    :mp_current,
+                                    :mp_past,
+                                    :use_average,
+                                    :past_use_yaer,
+                                    :use_mp_maker_name,
+                                    :mp_maker_name,
+                                    :use_earphone_maker,
+                                    :earphone_maker_name,
+                                    :ear_tone,
+                                    :ear_cause,
+                                    :other_ear_cause,
+                                    :comment,
+                                    :ear_age_result)
   end
 end
